7988c6709613c7bd47924c4ade8f5a91b0a3d910
vinay <vinaykhatri292@gmail.com>
2025-03-12 13:38:47 +0530

    Added socials

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -121,8 +121,8 @@ const Contact = () => {
               <i className='bx bx-mail-send contact__card-icon'></i>
 
               <h3 className='contact__card-title'>Email</h3>
-              <span className='contact__card-data'>rajatrathi029@gmail.com</span>
-              <a href='mailto: rajatrathi029@gmail.com' target="_blank" className='contact__button'> Write me{" "}
+              <span className='contact__card-data'>vinaykhatri292@gmail.com</span>
+              <a href='mailto: vinaykhatri292@gmail.com' target="_blank" className='contact__button'> Write me{" "}
                 <i className='bx bx-right-arrow-alt contact__button-icon'></i></a>
             </div>
 
@@ -130,17 +130,17 @@ const Contact = () => {
               <i className='bx bxl-whatsapp contact__card-icon'></i>
 
               <h3 className='contact__card-title'>Whatsapp</h3>
-              <span className='contact__card-data'>+91 97114 42850</span>
+              <span className='contact__card-data'>+91 9311842027</span>
               <a href='https://web.whatsapp.com/' target="_blank" className='contact__button'> Write me
                 <i className='bx bx-right-arrow-alt contact__button-icon'></i></a>
             </div>
 
             <div className='contact__card'>
-              <i className='bx bxl-twitter contact__card-icon'></i>
+              <i className='bx bxl-instagram contact__card-icon'></i>
 
-              <h3 className='contact__card-title'>Twitter</h3>
-              <span className='contact__card-data'>@iamvishalrathi1</span>
-              <a href='https://twitter.com/iamvishalrathi1' target="_blank" className='contact__button'> Write me
+              <h3 className='contact__card-title'>Instagram</h3>
+              <span className='contact__card-data'>@vinay_khatri29</span>
+              <a href='https://www.instagram.com/vinay_khatri29/' target="_blank" className='contact__button'> Write me
                 <i className='bx bx-right-arrow-alt contact__button-icon'></i>
               </a>
             </div>
